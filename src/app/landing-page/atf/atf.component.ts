05a6b89643457f9c1d5d57b8514905ba996766b9
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-07-09 13:04:43 +0200

    work on hover-effects

--- src/app/landing-page/atf/atf.component.ts
+++ src/app/landing-page/atf/atf.component.ts
@@ -2,10 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-atf',
+  standalone: true,
   imports: [],
   templateUrl: './atf.component.html',
   styleUrl: './atf.component.scss'
 })
 export class AtfComponent {
+  shapeImageSrc = '/img/hero section/shape.png';
+  arrowImageSrc = '/img/arrows/Arrow down.png';
 
+  onShapeMouseEnter() {
+    this.shapeImageSrc = '/img/hero%20section/Property%201=hover.png';
+  }
+
+  onShapeMouseLeave() {
+    this.shapeImageSrc = '/img/hero%20section/shape.png';
+  }
+
+  onArrowMouseEnter() {
+    this.arrowImageSrc = '/img/arrows/Arrow down hover.png';
+  }
+
+  onArrowMouseLeave() {
+    this.arrowImageSrc = '/img/arrows/Arrow down.png';
+  }
 }
