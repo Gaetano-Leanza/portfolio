ed6879e72368aef732219ca64c3a36e00bcf058d
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-07-11 17:18:02 +0200

    work on why me section

--- src/app/landing-page/why-me/why-me.component.ts
+++ src/app/landing-page/why-me/why-me.component.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-why-me',
   imports: [],
   templateUrl: './why-me.component.html',
-  styleUrl: './why-me.component.scss'
+  styleUrl: './why-me.component.scss',
 })
 export class WhyMeComponent {
+  whyMeSectionSrc = '/img/why me section/Button why-me.png';
 
+  whyMeMouseEnter() {
+    this.whyMeSectionSrc = '/img/why me section/Button why-me hover.png';
+  }
+
+  whyMeMouseLeave() {
+    this.whyMeSectionSrc = '/img/why me section/Button why-me.png';
+  }
 }
