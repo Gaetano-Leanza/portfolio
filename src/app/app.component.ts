e28d7e25ed9e95125afd6dd2b2664e6dcd816f14
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-07-11 08:12:08 +0200

    fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AtfComponent } from './landing-page/atf/atf.component'; 
+import { WhyMeComponent } from './landing-page/why-me/why-me.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AtfComponent],
+  imports: [RouterOutlet, AtfComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
