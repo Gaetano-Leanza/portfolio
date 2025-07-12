ebfcc574e1f39ed9eb4699f5f1b90790840333cb
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-07-12 11:46:37 +0200

    fix some bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AtfComponent } from './landing-page/atf/atf.component'; 
 import { WhyMeComponent } from './landing-page/why-me/why-me.component';
+import { MySkillsComponent } from "./landing-page/my-skills/my-skills.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AtfComponent, WhyMeComponent],
+  imports: [RouterOutlet, AtfComponent, WhyMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/landing-page/my-skills/my-skills.component.ts
+++ src/app/landing-page/my-skills/my-skills.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
+ellipseSrc = 'img/my skills/Ellipse.png';
+
+ ellipseEnter() {
+    this.ellipseSrc = 'img/my skills/Ellipse hover.png';
+  }
+
+  ellipseLeave() {
+    this.ellipseSrc = 'img/my skills/Ellipse english.png';
+  }
 
 }
