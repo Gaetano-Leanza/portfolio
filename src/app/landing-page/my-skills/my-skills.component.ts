2ae6d49acb4fc6f16658ab33a4fc2a3b3062d2eb
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-07-12 17:41:02 +0200

    finish why me section

--- src/app/landing-page/my-skills/my-skills.component.ts
+++ src/app/landing-page/my-skills/my-skills.component.ts
@@ -4,17 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-my-skills',
   imports: [],
   templateUrl: './my-skills.component.html',
-  styleUrl: './my-skills.component.scss'
+  styleUrl: './my-skills.component.scss',
 })
 export class MySkillsComponent {
-ellipseSrc = 'img/my skills/Ellipse.png';
+  ellipseSrc = 'img/my skills/Ellipse english.png';
 
- ellipseEnter() {
+  ellipseEnter() {
     this.ellipseSrc = 'img/my skills/Ellipse hover.png';
   }
 
   ellipseLeave() {
     this.ellipseSrc = 'img/my skills/Ellipse english.png';
   }
-
 }
