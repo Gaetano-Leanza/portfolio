493e465f8a20db5a772684e127705678d4b50052
Gaetano Leanza <leanzaundleanza@outlook.de>
2025-07-16 18:39:43 +0200

    wort on contact section

--- src/app/landing-page/contact-me/contact-me.component.ts
+++ src/app/landing-page/contact-me/contact-me.component.ts
@@ -2,8 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
-  standalone: true, // ← muss vorhanden sein!
+  standalone: true,
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss'],
 })
-export class ContactMeComponent {}
+export class ContactMeComponent {
+  EMailSrc: string = 'img/contact/🦆 icon _email_.png';
+  PhoneSrc: string = 'img/contact/🦆 icon _phone_.png';
+  ArrowSrc: string = 'img/contact/Arrow up.png';
+
+  EMailEnter(): void {
+    this.EMailSrc = 'img/contact/🦆 icon _email_hover.png';
+  }
+
+  EMailLeave(): void {
+    this.EMailSrc = 'img/contact/🦆 icon _email_.png';
+  }
+
+  PhoneEnter(): void {
+    this.PhoneSrc = 'img/contact/🦆 icon _phone_hover.png';
+  }
+
+  PhoneLeave(): void {
+    this.PhoneSrc = 'img/contact/🦆 icon _phone_.png';
+  }
+
+  arrowEnter(): void {
+    this.ArrowSrc = 'img/contact/Arrow up hover.png';
+  }
+
+  arrowLeave(): void {
+    this.ArrowSrc = 'img/contact/Arrow up.png';
+  }
+}
